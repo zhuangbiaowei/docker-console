f6d17d764c7e6fcf1899ed37d8f41fdfb1f1e767
庄表伟 <zhuangbiaowei@gmail.com>
2015-11-18 22:23:07 +0800

    add some func

--- ripl/like_shell.rb
+++ ripl/like_shell.rb
@@ -3,7 +3,12 @@ module Ripl::Readline
     input = Readline.readline prompt, true
     if input.include?(" ")
       arr = input.split(" ")
-      return arr[0]+" "+arr[1..-1].collect{|k| "\"#{k}\""}.join(",")
+      params = DockerConsole.instance_method(arr[0].to_sym).parameters
+      if arr.length-1 > params.length
+        return arr[0] + " " + arr[1..params.length-1].collect{|k| "\"#{k}\""}.join(",") +","+ "[#{arr[params.length..-1].collect{|k| "\"#{k}\""}.join(",")}]"
+      else
+        return arr[0]+" "+arr[1..-1].collect{|k| "\"#{k}\""}.join(",")
+      end
     else
       return input
     end
