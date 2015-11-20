a7d2006899b763fcff08d3ea664cf7c4ba5b1046
庄表伟 <zhuangbiaowei@gmail.com>
2015-11-20 23:01:14 +0800

    good multi run

--- lib/command/container.rb
+++ lib/command/container.rb
@@ -15,44 +15,18 @@ module Container
     container.delete
     "Container #{id} Deleted!"
   end
-  def run_rm(img,cmd=nil)
-    if cmd==nil
-      cmd = []
-    elsif cmd.class == String
-      cmd = [cmd]
-    end
-    container = Docker::Container.create('Image' => img, 'Cmd' => cmd)
-    container.start
-    container = Docker::Container.get(container.id)
-    while container.info["State"]["Status"]=="running"
-      sleep 1
-      container = Docker::Container.get(container.id)
-    end
-    puts container.logs(stdout: true,stderr: true)
-    container.stop
-    container.kill
-    container.delete(:force => true)
+  def run(img,cmd=[])
+    run_container(img,cmd)
   end
-  def run(img,cmd=nil)
-    if cmd==nil
-      cmd = []
-    elsif cmd.class == String
-      cmd = [cmd]
-    end
-    container = Docker::Container.create('Image' => img, 'Cmd' => cmd)
-    container.start
-  end
-  def mrun(img,cmd=nil)
-    if cmd==nil
-      cmd = []
-    elsif cmd.class == String
-      cmd = [cmd]
-    end
+  def mrun(img,cmd=[])
     url = Docker.url
     @machines.each do |machine|
+      ENV["DOCKER_TLS_VERIFY"]="1"
+      ENV["DOCKER_HOST"]=machine["url"]
+      ENV["DOCKER_CERT_PATH"]=File.expand_path(@docker_machine_data)+"/"+machine["name"]
+      ENV["DOCKER_MACHINE_NAME"]=machine["name"]
       Docker.url = machine["url"]
-      container = Docker::Container.create('Image' => img, 'Cmd' => cmd)
-      container.start
+      run_container(img,cmd.clone)
     end
     Docker.url = url
     return "Run command at #{@machines.count} machines."
--- lib/function/run_container.rb
+++ lib/function/run_container.rb
@@ -1,3 +1,25 @@
-def run(docker,image,cmd)
-  
+def run_container(img,cmd=[])
+  if cmd.class == String
+    cmd = [cmd]
+  end
+  if cmd.include?("--rm")
+    rm=true
+    cmd.delete("--rm")
+  end
+  container = Docker::Container.create('Image' => img, 'Cmd' => cmd)
+  container.start
+  if rm==true
+    while container.info["State"]==nil
+      sleep 1
+      container = Docker::Container.get(container.id)
+    end
+    while container.info["State"]["Status"]=="running"
+      sleep 1
+      container = Docker::Container.get(container.id)
+    end
+    puts container.logs(stdout: true,stderr: true)
+    container.stop
+    container.kill
+    container.delete(:force => true)
+  end
 end
